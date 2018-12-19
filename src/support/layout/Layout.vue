bf9fe4252f11e9981c8bab64ef5530d6c383d977
Aldrovando CAMARGO Neves <aldrov_camargo@hotmail.com>
2018-12-18 22:57:38 -0200

    Side Menu started
    Side Menu started (draft)

--- src/support/layout/Layout.vue
+++ src/support/layout/Layout.vue
@@ -1,17 +1,36 @@
-<template lang="pug">
-  v-app
-    v-navigation-drawer(
-      app
-      clipped
-      v-model="drawer"
-    )
-    v-toolbar(app color="amber")
-      v-toolbar-side-icon(@click.stop="setDrawer()")
-      span Application
-    v-content
-      v-container(fluid)
-        router-view
-    v-footer(app)
+<template>
+  <v-app>
+    <v-toolbar app color="amber">
+      <v-toolbar-side-icon @click.stop="setDrawer()"></v-toolbar-side-icon>
+      <span>Clientes</span>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid tag="router-view"></v-container>
+    </v-content>
+    <v-footer>app</v-footer>
+    <v-navigation-drawer
+        class="blue"
+        dark
+        clipped
+        app
+        v-model="drawer"
+    >
+        <v-list>
+          <v-list-tile
+            v-for="item in items"
+            :key="item.title"
+            @click=""
+          >
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+    </v-navigation-drawer>
+  </v-app>
 </template>
 
 <script>
@@ -19,6 +38,11 @@ export default {
   data() {
     return {
       drawer: false,
+      items: [
+        { title: 'Dashboard', icon: 'dashboard' },
+        { title: 'Account', icon: 'account_box' },
+        { title: 'Admin', icon: 'gavel' },
+      ],
     };
   },
   methods: {
